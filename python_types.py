2bc2eb810bd2c1a2b7fbe543077842a27608e086
EimantasBlazevicius <eima.blaz@gmail.com>
2023-04-23 11:40:22 +0300

    Initial code upload

---
+++ python_types.py
@@ -0,0 +1,14 @@
+# string, str
+print(type("Hello world!"))
+
+# integer, int
+print(type(2))
+
+# float
+print("159.100")
+
+# Boolean, bool
+print(type(True))
+
+# None type
+print(type(None))
---
+++ python_variables_operations.py
@@ -0,0 +1,53 @@
+number = 0
+number2 = 0
+name = "Name"
+some_text = "195 meters"
+a_number_with_dot = 1.1
+logical_value = True
+
+# Arithmetic operators
+print(1+1)
+print(7-2)
+print(2*2)
+print(2**4)
+print(9/3)
+print(9//2.9)
+print(9 % 3)
+
+# Comparison operators
+print(1==2)
+print(1!=2)
+print(1>2)
+print(1<2)
+print(1>=2)
+print(1<=1)
+
+# Assignment operators
+simple = "simple"
+number += 5
+print(number)
+number -= 3
+print(number)
+number *= 7
+print(number)
+number **= 7
+print(number)
+number /= 9700
+print(number)
+
+# Identity operators
+print(number is not number2)
+print(number is number2)
+
+# Logical operators
+print(number is number2 and number is not number)
+
+# Membership operators
+print('x' not in some_text)
+
+# Combining variables
+print('The result of this text is ' + some_text + ' conducted by '+ name)
+print(f"The result of this text is {some_text} conducted by {name}")
+
+print((name+' ')*5)
+print(name + number)
