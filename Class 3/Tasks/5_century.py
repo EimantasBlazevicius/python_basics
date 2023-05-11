a15c1ce118d88bdcf092b78d2d06a5a44790293c
EimantasBlazevicius <eima.blaz@gmail.com>
2023-05-11 07:04:18 +0300

    fix

--- Class 3/Tasks/5_century.py
+++ Class 3/Tasks/5_century.py
@@ -2,7 +2,5 @@ year = int(input("Enter the year: "))
 
 if year == 0:
     print("NICE! You are not even in century  category yet")
-elif year <= 100:
-    print(f"You are in the 1 century")
 else:
-    print(f"Your are in the {(year-1) // 100} century")
+    print(f"Your are in the {((year-1) // 100) + 1} century")
